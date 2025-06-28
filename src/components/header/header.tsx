27e0be05257c0a02371b680e120a1eec7e173fbd
TinCanAustin <austin.bijumon@gmail.com>
2025-06-27 18:30:07 -0700

    Header component, page.tsx clean up, global.css clean up

---
+++ src/components/header/header.tsx
@@ -0,0 +1,14 @@
+import './style.css'
+
+function Header(){
+    return(
+        <div id='header'>
+            <p id='head'>HIGHLIGHTS</p>
+            <h1 id='title'>What are the special moments of your life?</h1>
+            <p id='disc'>We believe every momment counts! Share your favorite highlights, 
+                unforgettable memories, and the stories that make your life shine.</p>
+        </div>
+    );
+}
+
+export default Header;
