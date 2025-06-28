32e3a98de365a92e63345e269b615a8a440e24a6
TinCanAustin <austin.bijumon@gmail.com>
2025-06-27 20:47:36 -0700

    Highlight Card Function created

---
+++ src/components/highlight_card/highlight_card.tsx
@@ -0,0 +1,12 @@
+function Highlight_card({name, location, desc, img_id}:{
+    name: string,
+    location: string,
+    desc: string,
+    img_id?: string
+}){
+    return(
+        <></>
+    );
+}
+
+export default Highlight_card;
