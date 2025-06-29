db183712fff5a86d8fdcbcc1de5aa4749bed63fe
TinCanAustin <austin.bijumon@gmail.com>
2025-06-29 07:51:44 -0700

    Base Highlight card created

--- src/components/highlight_card/highlight_card.tsx
+++ src/components/highlight_card/highlight_card.tsx
@@ -1,3 +1,6 @@
+import no_img_found from "../../../public/no_img_found.png"
+import './style.css'
+
 function Highlight_card({name, location, desc, img_id}:{
     name: string,
     location: string,
@@ -5,7 +8,14 @@ function Highlight_card({name, location, desc, img_id}:{
     img_id?: string
 }){
     return(
-        <></>
+        <div className="highlight_card">
+            <img src={
+                img_id ? img_id : no_img_found.src
+            } alt="No Image Found" className="image_parm"/>
+            <h3 className="higlight_title">{name}</h3>
+            <p className="higlight_loc">{location}</p>
+            <p className="higlight_desc">{desc}</p>
+        </div>
     );
 }
 
