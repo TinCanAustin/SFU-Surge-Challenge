ec62e7dddbdc734b9cc81a035e8ed77de41d7e14
TinCanAustin <austin.bijumon@gmail.com>
2025-06-29 20:43:59 -0700

    Create a Paralle Route for create_highlight.tsx

---
+++ src/app/@modal/create/page.tsx
@@ -0,0 +1,9 @@
+import Create_highlight from "@/components/create_highlight/create_highlight";
+
+export default function CreateHighlight() {
+    return(
+        <>
+            <Create_highlight/>
+        </>
+    );
+}
