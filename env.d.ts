d704744d321210f9641a0a63c3bb4f1aff15f63a
TinCanAustin <austin.bijumon@gmail.com>
2025-06-29 23:48:30 -0700

    Changing env var name and adding no-cache

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,7 @@
 declare namespace NodeJS {
     interface ProcessEnv {
-        GOOGLE_API_KEY : string
-        SEARCH_KEY : string
+        NEXT_PUBLIC_GOOGLE_API_KEY : string
+        NEXT_PUBLIC_SEARCH_KEY : string
         NEXT_PUBLIC_SECRET : string
     }
 }
--- src/functions/searchImage.ts
+++ src/functions/searchImage.ts
@@ -3,8 +3,8 @@ import { search_tp } from "@/types/search.dto";
 export async function search(img_src : string){
     try{
         const response = await fetch("https://www.googleapis.com/customsearch/v1?" + new URLSearchParams({
-            key: process.env.GOOGLE_API_KEY,
-            cx: process.env.SEARCH_KEY,
+            key: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
+            cx: process.env.NEXT_PUBLIC_SEARCH_KEY,
             q: img_src,
             searchType: 'image',
             num: '5'
